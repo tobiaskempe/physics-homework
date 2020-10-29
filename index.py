63de952a87a9f5b102c20cc671690560f4be94e0
Tobias Kempe <tkempe96@gmail.com>
2020-10-29 18:36:02 +0100

    solved exercise a)

---
+++ index.py
@@ -0,0 +1,34 @@
+
+import sys
+import random
+import math
+
+def shoot_pi(num_samples = 10, verbose = True):
+
+    if verbose:
+        print('Shooting PI with ' + str(num_samples) + ' samples...')
+    
+    # shoot randomly (uniform) at 1x1 area at count hits of inner quarter circle.
+    num_hits = 0
+    for _ in range(num_samples):
+        x = random.random()
+        y = random.random()
+        if (math.sqrt(math.pow(x, 2) + math.pow(y, 2)) < 1):
+            num_hits += 1
+    
+    # compute pi as ratio of hits to all samples
+    # (factor 4 necessary, since we're only shooting at a quarter circle)
+    pi = (num_hits / num_samples) * 4 
+
+    if verbose:
+        print('PI is approximately ', str(pi), '!', sep='')
+    
+    return pi
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        shoot_pi(int(sys.argv[1]))
+    else:
+        shoot_pi()
+
