de4546acebb7f5209517c0b9be2235fa3e66f101
Tobias Kempe <tkempe96@gmail.com>
2020-10-30 14:50:58 +0100

    removed unnecessary comments

--- show_dependency.py
+++ show_dependency.py
@@ -19,17 +19,13 @@ def show_dependency():
     fig, axs = plt.subplots(2)
     fig.canvas.set_window_title('Homework 1')
     fig.suptitle(r'Showing: $\Delta x \sim \frac{1}{\sqrt{N}}$, M=' + str(M))
-    #fig.tight_layout(pad=3)
     axs[0].plot(Ns, np.array(results)[:, 1])
     axs[0].set_xlabel('N')
     axs[0].set_ylabel(r'$\Delta x$', rotation=0)
-    #plt.show()
     axs[1].plot(1/np.sqrt(np.array(Ns)), np.array(results)[:, 1])
     axs[1].set_xlabel(r'$\frac{1}{\sqrt{N}}$')
     axs[1].set_ylabel(r'$\Delta x$', rotation=0)
     plt.show()
-    #plt.title('1/sqrt(N) ~ delta_x')
-    #plt.show()
 
 
 if __name__ == '__main__':
