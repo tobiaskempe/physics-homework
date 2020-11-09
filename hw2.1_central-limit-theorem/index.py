fbb9b270afed4e12d358754ba538c0927979899f
Tobias Kempe <tkempe96@gmail.com>
2020-11-09 13:20:43 +0100

    HW2.1: removed old code fragments

--- hw2.1_central-limit-theorem/index.py
+++ hw2.1_central-limit-theorem/index.py
@@ -23,38 +23,17 @@ def sample_Y(M, N, distribution):
         y_samples.append(Y(N, distribution))
     return np.array(y_samples)
 
-def viz_Y(ys):
-    fig, axs = plt.subplots(3)
-    for i in range(3):
-        axs[i].hist(ys[i], bins=200)
-    plt.show()
-
 if __name__ == '__main__':
 
     M = 10_000
 
     dists = (random_binary, random_uniform, random_poisson)
-    Ns = (1, 10, 100, 1000, 1000)
 
-    fig, axs = plt.subplots(5, 3)
+    _, axs = plt.subplots(5, 3)
 
     for i in range(5):
-        # N = int(np.power(np.power(100, 1/4), i))
-        N = Ns[i]
-        print(N)
+        N = int(np.power(np.power(100, 1/4), i))
         for j in range(3):
             sample = sample_Y(M, N, dists[j])
             axs[i][j].hist(sample, bins=200)
     plt.show()
-
-
-    # ys = []
-    # ys.append(sample_Y(M, N, random_binary))
-    # ys.append(sample_Y(M, N, random_uniform))
-    # ys.append(sample_Y(M, N, random_poisson))
-
-    # viz_Y(ys)
-        
-
-        
-
